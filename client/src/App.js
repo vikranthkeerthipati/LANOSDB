a85244f0e46a053b1ebb1ab236973ee9da4548dd
Mihir Bafna <mihir.bafna@gmail.com>
2020-10-31 23:39:09 -0400

    added new client app

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import { Form, Modal, Container, Button } from 'react-bootstrap'
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+
+      <div className="App">
+        <Modal.Dialog>
+          <Modal.Header>
+            <Modal.Title>Organization Database Form</Modal.Title>
+          </Modal.Header>
+          <Form>
+            <Modal.Body>
+              <Container style={{ textAlign: "left" }}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+              </Container>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" type="submit">Submit</Button>
+            </Modal.Footer>
+          </Form>
+        </Modal.Dialog>
+
+      </div>
+    );
+  }
+}
+
+export default App;
